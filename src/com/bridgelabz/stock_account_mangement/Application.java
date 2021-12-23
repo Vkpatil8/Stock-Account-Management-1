cb24b1c3a7957b71610969635dc6a79b8ca47ec9
vkpatil <vishalvkpatil8@gmail.com>
2021-12-23 18:31:20 +0530

    File added

---
+++ src/com/bridgelabz/stock_account_mangement/Application.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stock_account_mangement;
+
+public class Application {
+    public static void main(String[] args) {
+        ShowMenu showMenu = new ShowMenu();
+        showMenu.Menu();
+    }
+}
